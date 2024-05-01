dc8f66bd4b878ef2d83255ff061873c6e4ec3e2c
Vicente Lourenço <134103080+VicLourenco40@users.noreply.github.com>
2024-05-01 20:14:59 +0100

    Add contacts page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const buttonTop = document.getElementById('button-top');
+
+function showButtonTop(show) {
+    let visibility = show ? "visible" : "hidden";
+
+    buttonTop.style.visibility = visibility;
+}
+
+window.onscroll = function() {
+    showButtonTop(document.documentElement.scrollTop > 0)
+}
